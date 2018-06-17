55a09af7c31b2f6672b339c6a6804e6d05958766
unknown <remotephone@gmail.com>
2018-06-16 23:23:07 -0500

    more work

--- part1/billingalarm.py
+++ part1/billingalarm.py
@@ -7,52 +7,94 @@ import argparse
 import datetime
 import json
 import sys
+import logging as log
 
 import boto3
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Create an IAM user.')
 parser.add_argument('-a', '--alarmname', required=True, help='Name your billing alarm')
+parser.add_argument('-c', '--costpermonth', required=True, help='Trigger alarm when charges meet this value in $USD')
+parser.add_argument('-d', '--alarmdescription', required=True, help='Describe your alarm in "\'s')
 
 args = parser.parse_args()
 
 alarmname = args.alarmname
-username = args.username
-delete = args.delete
-create = args.create
+costpermonth = args.costpermonth
+alarmdescription = args.alarmdescription
 
 
-client = boto3.client('cloudwatch')
+# I want to query available sns topics and provide them as a list to the user
 
-rresponse = client.put_metric_alarm(
-    AlarmName='string',
-    AlarmDescription='string',
-    ActionsEnabled=True,
-    OKActions=[
-        'string',
-    ],
-    AlarmActions=[
-        'string',
-    ],
-    InsufficientDataActions=[
-        'string',
-    ],
-    MetricName='string',
-    Namespace='string',
-    Statistic='SampleCount'|'Average'|'Sum'|'Minimum'|'Maximum',
-    ExtendedStatistic='string',
-    Dimensions=[
-        {
-            'Name': 'Currency',
-            'Value': 'USD'
-        },
-    ],
-    Period=21600,
-    Unit='Seconds'|'Microseconds'|'Milliseconds'|'Bytes'|'Kilobytes'|'Megabytes'|'Gigabytes'|'Terabytes'|'Bits'|'Kilobits'|'Megabits'|'Gigabits'|'Terabits'|'Percent'|'Count'|'Bytes/Second'|'Kilobytes/Second'|'Megabytes/Second'|'Gigabytes/Second'|'Terabytes/Second'|'Bits/Second'|'Kilobits/Second'|'Megabits/Second'|'Gigabits/Second'|'Terabits/Second'|'Count/Second'|'None',
-    EvaluationPeriods=123,
-    DatapointsToAlarm=123,
-    Threshold=123.0,
-    ComparisonOperator='GreaterThanOrEqualToThreshold'|'GreaterThanThreshold'|'LessThanThreshold'|'LessThanOrEqualToThreshold',
-    TreatMissingData='string',
-    EvaluateLowSampleCountPercentile='string'
-)
+def get_topics():
+    gottopics = []
+    snsclient = boto3.client('sns')
+    snstopics = snsclient.list_topics()
+
+    for topic in snstopics['Topics']:
+        gottopics.append(topic)
+    print(gottopics)
+    itemcount = len(gottopics)
+    print(itemcount)
+    rangecount = range(1, itemcount)
+    print(enumerate(rangecount))
+    optionslist = dict(zip(enumerate(rangecount), snstopics['Topics']))
+    print(optionslist)
+
+
+
+def check_alarms():
+    cwclient = boto3.client('cloudwatch')
+    try:
+        alarmsnow = cwclient.describe_alarms()
+        for alarm in alarmsnow['MetricAlarms']['AlarmName']:
+            print(alarm)
+    except:
+        log.error("That didnt work")
+
+
+
+
+
+
+def create_alarm(alarmname, alarmdescription, snstopic, costpermonth):
+    cwclient = boto3.client('cloudwatch')
+    resp = cwclient.put_metric_alarm(
+        AlarmName=alarmname,
+        AlarmDescription=alarmdescription,
+        ActionsEnabled=True,
+        OKActions=[
+            snstopic,
+        ],
+        AlarmActions=[
+            snstopic,
+        ],
+        InsufficientDataActions=[
+            snstopic,
+        ],
+        MetricName='EstimatedCharges',
+        Namespace='AWS/Billing',
+        Statistic='Maximum',
+        Dimensions=[
+            {
+                'Name': 'Currency',
+                'Value': 'USD'
+            },
+        ],
+        Period=21600,
+        EvaluationPeriods=1,
+        Threshold=float(costpermonth),
+        ComparisonOperator='GreaterThanOrEqualToThreshold',
+        TreatMissingData='ignore',
+    )
+
+    return resp
+
+
+get_topics()
+check_alarms()
+
+# snstopic = input("What's the full of ARN of the topic you want to alert to?\n> ")
+# resp = create_alarm(alarmname, alarmdescription, snstopic, costpermonth)
+
+# print(resp) 
