31f7c8bd327b14e71a356bc4e8a3b4d58834642e
unknown <remotephone@gmail.com>
2018-06-20 00:47:25 -0500

    Not working

--- part1/billingalarm.py
+++ part1/billingalarm.py
@@ -7,52 +7,92 @@ import argparse
 import datetime
 import json
 import sys
+import logging as log
 
 import boto3
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='Create an IAM user.')
 parser.add_argument('-a', '--alarmname', required=True, help='Name your billing alarm')
+parser.add_argument('-c', '--costpermonth', required=True, help='Trigger alarm when charges meet this value in $USD')
+parser.add_argument('-d', '--alarmdescription', required=True, help='Describe your alarm in "\'s')
+parser.add_argument('-s', '--snstopic', required=False, help='Full ARN of SNS Topic to publish to')
 
 args = parser.parse_args()
 
 alarmname = args.alarmname
-username = args.username
-delete = args.delete
-create = args.create
+costpermonth = args.costpermonth
+alarmdescription = args.alarmdescription
+snstopic = args.snstopic
+
+# I want to query available sns topics and provide them as a list to the user
+
+def get_topics():
+    gottopics = []
+    snsclient = boto3.client('sns')
+    snstopics = snsclient.list_topics()
+    print("Which topic do you want to send notifications to?")
+
+    for topic in snstopics['Topics']:
+        gottopics.append(topic)
+        print("- " + topic['TopicArn'])
+    # itemcount = len(gottopics)
+    # rangecount = range(0, itemcount)
+    # optionslist = dict(zip(rangecount), snstopics['Topics'])
+    # print(optionslist)
+    # return optionslist
+    return gottopics
 
 
-client = boto3.client('cloudwatch')
 
-rresponse = client.put_metric_alarm(
-    AlarmName='string',
-    AlarmDescription='string',
-    ActionsEnabled=True,
-    OKActions=[
-        'string',
-    ],
-    AlarmActions=[
-        'string',
-    ],
-    InsufficientDataActions=[
-        'string',
-    ],
-    MetricName='string',
-    Namespace='string',
-    Statistic='SampleCount'|'Average'|'Sum'|'Minimum'|'Maximum',
-    ExtendedStatistic='string',
-    Dimensions=[
-        {
-            'Name': 'Currency',
-            'Value': 'USD'
-        },
-    ],
-    Period=21600,
-    Unit='Seconds'|'Microseconds'|'Milliseconds'|'Bytes'|'Kilobytes'|'Megabytes'|'Gigabytes'|'Terabytes'|'Bits'|'Kilobits'|'Megabits'|'Gigabits'|'Terabits'|'Percent'|'Count'|'Bytes/Second'|'Kilobytes/Second'|'Megabytes/Second'|'Gigabytes/Second'|'Terabytes/Second'|'Bits/Second'|'Kilobits/Second'|'Megabits/Second'|'Gigabits/Second'|'Terabits/Second'|'Count/Second'|'None',
-    EvaluationPeriods=123,
-    DatapointsToAlarm=123,
-    Threshold=123.0,
-    ComparisonOperator='GreaterThanOrEqualToThreshold'|'GreaterThanThreshold'|'LessThanThreshold'|'LessThanOrEqualToThreshold',
-    TreatMissingData='string',
-    EvaluateLowSampleCountPercentile='string'
-)
+def check_alarms(gottopics):
+    cwclient = boto3.client('cloudwatch')
+    try:
+        alarmsnow = cwclient.describe_alarms()
+        print(alarmsnow)
+        for k, v  in alarmsnow['MetricAlarms']['AlarmName']:
+            print(v)
+    except:
+        log.error("That didnt work")
+
+
+def create_alarm(alarmname, alarmdescription, snstopic, costpermonth):
+    cwclient = boto3.client('cloudwatch')
+    resp = cwclient.put_metric_alarm(
+        AlarmName=alarmname,
+        AlarmDescription=alarmdescription,
+        ActionsEnabled=True,
+        OKActions=[
+            snstopic,
+        ],
+        AlarmActions=[
+            snstopic,
+        ],
+        InsufficientDataActions=[
+            snstopic,
+        ],
+        MetricName='EstimatedCharges',
+        Namespace='AWS/Billing',
+        Statistic='Maximum',
+        Dimensions=[
+            {
+                'Name': 'Currency',
+                'Value': 'USD'
+            },
+        ],
+        Period=21600,
+        EvaluationPeriods=1,
+        Threshold=float(costpermonth),
+        ComparisonOperator='GreaterThanOrEqualToThreshold',
+        TreatMissingData='ignore',
+    )
+
+    return resp
+
+if snstopic:
+    gottopics = snstopic
+else:
+    gottopics = get_topics()
+
+print("now check_alarms")
+check_alarms(gottopics)
