d36ef6908e53d383b3bbab87832f3749f7571cdd
computer <remotephone@gmail.com>
2018-07-31 22:54:23 -0400

    cleaned it up and broke it

--- aws-sts.py
+++ aws-sts.py
@@ -5,38 +5,31 @@ import sys
 import os 
 from pathlib import Path, PurePath
 
-parser = ArgumentParser()
-profile = parser.add_argument("-p", "--profile", help="Profile name, defaults to default", default="default")
-duration = parser.add_argument("-d", "--duration", help="How long the token lasts, in hours - defaults to 4hrs/14400 seconds, max 36hrs", default="14400")
-serial = parser.add_argument("-s", "--serial", help="SerialNumber for MFA token - ", default=None)
-token = parser.add_argument("-t", "--token", help="MFA token from your app", required=True)
-args = parser.parse_args()
-
-profile = args.profile
-duration = args.duration
-token = args.token
-config = ConfigParser()
-awscreds = Path.home().joinpath('.aws', 'credentials')
-config.read(awscreds)
+# parser = ArgumentParser()
+# profile = parser.add_argument("-p", "--profile", help="Profile name, defaults to default", default="default")
+# duration = parser.add_argument("-d", "--duration", help="How long the token lasts, in hours - defaults to 4hrs/14400 seconds, max 36hrs", default="14400")
+# serial = parser.add_argument("-s", "--serial", help="SerialNumber for MFA token - ", default=None)
+# token = parser.add_argument("-t", "--token", help="MFA token from your app", required=True)
+# region = parser.add_argument("-r", "--region", help="Default region, defaults to us-east-1", default='us-east-1')
+# args = parser.parse_args()
 
 
-def get_serial(profile):
+ 
+def get_serial(serial, awscreds, profile):
     try:
-        if args.serial:
-            serial = args.serial
-        else:
-            config.read(awscreds)
-            serial = config.get(profile, 'mfa_serial')
-            return serial
+        config = ConfigParser()
+        config.read(awscreds)
+        serial = config.get(profile, 'mfa_serial')
+        print(serial)
+        return serial
     except:
-        print('No MFA Serial Configured!')
+        print('No MFA Serial Configured for ' + profile + ' profile!')
         sys.exit(1)
 
 
-def get_token(serial):
+def get_token(profile, duration, serial, token):
     session = boto3.session.Session(profile_name=profile)
     client = session.client('sts')
-
     try:
         response = client.get_session_token(
             DurationSeconds=int(duration),
@@ -51,12 +44,15 @@ def get_token(serial):
         print('oops')
 
 
-def update_config(profile, acckey, seckey, temptok):
+def update_creds(profile, awscreds, acckey, seckey, temptok):
     mfaprofname = profile + '-mfa'
+    config = ConfigParser()
+    config.read(awscreds)
     if config.has_section(mfaprofname) == True:
         config.set(mfaprofname, 'aws_access_key_id', acckey)
         config.set(mfaprofname, 'aws_secret_access_key', seckey)
         config.set(mfaprofname, 'aws_session_token', temptok)
+        config.set()
     else:
         config.add_section(mfaprofname)
         config.set(mfaprofname, 'aws_access_key_id', acckey)
@@ -65,6 +61,17 @@ def update_config(profile, acckey, seckey, temptok):
     with open(awscreds, 'w') as configfile:
         config.write(configfile)
 
+def update_config(profile, region):
+
+    awsconfig = Path.home().joinpath('.aws', 'confog')
+    config.read(awsconfig)
+    if config.has_section(mfaprofname) == True:
+        config.set(mfaprofname, 'default-region', region)
+    else:
+        config.add_section(mfaprofname)
+        config.set(mfaprofname, 'default-region', region)
+
+
 
 def main():
     parser = ArgumentParser()
@@ -72,20 +79,23 @@ def main():
     duration = parser.add_argument("-d", "--duration", help="How long the token lasts, in hours - defaults to 4hrs/14400 seconds, max 36hrs", default="14400")
     serial = parser.add_argument("-s", "--serial", help="SerialNumber for MFA token - ", default=None)
     token = parser.add_argument("-t", "--token", help="MFA token from your app", required=True)
+    region = parser.add_argument("-r", "--region", help="Default region, defaults to us-east-1", default='us-east-1')
     args = parser.parse_args()
 
     profile = args.profile
     duration = args.duration
     token = args.token
+    serial = args.serial
+    config = ConfigParser()
+    awscreds = Path.home().joinpath('.aws', 'credentials')
+    config.read(awscreds)
 
-
-    if args.serial:
-        serial = args.serial
-    else:
-        serial = get_serial(profile)
-
-    acckey, seckey, temptok = get_token(serial)
-    update_config(profile, acckey, seckey, temptok)
+    serial = get_serial(serial, awscreds, profile)
+    print(serial)
+    print(profile)
+    acckey, seckey, temptok = get_token(profile, duration, serial, token)
+    update_creds(profile, awscreds, acckey, seckey, temptok)
+    update_config(profile, region)
 
 
 
