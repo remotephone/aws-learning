8cec969641133e890fe510d005d90bb35a59acba
computer <remotephone@gmail.com>
2018-08-06 19:54:00 -0500

    working sts script

--- aws-sts.py
+++ aws-sts.py
@@ -4,23 +4,12 @@ from configparser import ConfigParser
 import sys
 import os 
 from pathlib import Path, PurePath
-
-# parser = ArgumentParser()
-# profile = parser.add_argument("-p", "--profile", help="Profile name, defaults to default", default="default")
-# duration = parser.add_argument("-d", "--duration", help="How long the token lasts, in hours - defaults to 4hrs/14400 seconds, max 36hrs", default="14400")
-# serial = parser.add_argument("-s", "--serial", help="SerialNumber for MFA token - ", default=None)
-# token = parser.add_argument("-t", "--token", help="MFA token from your app", required=True)
-# region = parser.add_argument("-r", "--region", help="Default region, defaults to us-east-1", default='us-east-1')
-# args = parser.parse_args()
-
-
  
 def get_serial(serial, awscreds, profile):
     try:
         config = ConfigParser()
         config.read(awscreds)
         serial = config.get(profile, 'mfa_serial')
-        print(serial)
         return serial
     except:
         print('No MFA Serial Configured for ' + profile + ' profile!')
@@ -41,8 +30,8 @@ def get_token(profile, duration, serial, token):
         temptok = response['Credentials']['SessionToken']
         return acckey, seckey, temptok
     except:
-        print('oops')
-
+        print('[!] - Something went wrong, check your MFA token and serial, ' + serial)
+        sys.exit(1)
 
 def update_creds(profile, awscreds, acckey, seckey, temptok):
     mfaprofname = profile + '-mfa'
@@ -52,7 +41,6 @@ def update_creds(profile, awscreds, acckey, seckey, temptok):
         config.set(mfaprofname, 'aws_access_key_id', acckey)
         config.set(mfaprofname, 'aws_secret_access_key', seckey)
         config.set(mfaprofname, 'aws_session_token', temptok)
-        config.set()
     else:
         config.add_section(mfaprofname)
         config.set(mfaprofname, 'aws_access_key_id', acckey)
@@ -62,9 +50,10 @@ def update_creds(profile, awscreds, acckey, seckey, temptok):
         config.write(configfile)
 
 def update_config(profile, region):
-
-    awsconfig = Path.home().joinpath('.aws', 'confog')
+    config = ConfigParser()
+    awsconfig = Path.home().joinpath('.aws', 'config')
     config.read(awsconfig)
+    mfaprofname = profile + '-mfa'
     if config.has_section(mfaprofname) == True:
         config.set(mfaprofname, 'default-region', region)
     else:
@@ -86,16 +75,19 @@ def main():
     duration = args.duration
     token = args.token
     serial = args.serial
+    region = args.region
+
     config = ConfigParser()
+
     awscreds = Path.home().joinpath('.aws', 'credentials')
     config.read(awscreds)
+    print(profile)
 
     serial = get_serial(serial, awscreds, profile)
-    print(serial)
-    print(profile)
     acckey, seckey, temptok = get_token(profile, duration, serial, token)
     update_creds(profile, awscreds, acckey, seckey, temptok)
     update_config(profile, region)
+    print('Configured profile "' + profile + '-mfa" to use "' + region + '" region')
 
 
 
