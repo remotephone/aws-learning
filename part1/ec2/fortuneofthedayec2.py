4756c2f575067b8349b9a09ffa6d6a183132f1ed
computer <remotephone@gmail.com>
2018-10-17 23:57:02 -0500

    minor progress

--- part1/ec2/fortuneofthedayec2.py
+++ part1/ec2/fortuneofthedayec2.py
@@ -1,11 +1,23 @@
+import argparse
 import boto3
 import json
 import os
 import requests
 
 
-ec2client = boto3.client('ec2')
-ec2 = boto3.resource('ec2', region_name='us-east-1')
+parser = argparse.ArgumentParser(description='Quick and dirty copy one bucket to another.')
+parser.add_argument('-p', '--profile', required=True, help='profile name from aws creds')
+
+args = parser.parse_args()
+
+profile = args.profile 
+
+session = boto3.Session(profile_name=profile)
+
+
+ec2client = session.client('ec2', region_name='us-east-1')
+
+ec2 = session.resource('ec2', region_name='us-east-1')
 instance = ec2.Instance('id')
 
 def get_my_ip():
@@ -15,7 +27,7 @@ def get_my_ip():
 
 myip = get_my_ip()
 
-secgroup_name = 'test_groupwow'
+secgroup_name = 'test_groupwow2'
 
 groups_now = ec2client.describe_security_groups()
 
@@ -28,49 +40,54 @@ motd_userdata = """#!/bin/bash
     """
 
 def get_instance_profile():
-    iamclient = boto3.client('iam')
+    iamclient = session.client('iam')
     ec2profiles = iamclient.list_instance_profiles()
     for ec2profile in ec2profiles['InstanceProfiles']:
         print(ec2profile['InstanceProfileName'])
 
 
 
-def create_new_inst():
+def create_new_inst(sec_group):
     instance = ec2.create_instances(ImageId='ami-759bc50a', InstanceType="t2.micro", 
         MaxCount=1, MinCount=1, SecurityGroupIds=[sec_group.id], user_data=motd_userdata,
         IamInstanceProfile={'Name': 'ec2_s3_motd_role'})
+    return instance
 
 
 # need to test if group exists and handle that correctly. 
-def handle_sec_groups():
+def handle_sec_groups(secgroup_name):
     checkedgroups = []
+    print('---')
     print(groups_now)
     for group_now in groups_now['SecurityGroups']:
         print(group_now['GroupName'])
-        checkedgroups = checkedgroups.append(group_now['GroupName'])
+        checkedgroups.append(group_now['GroupName'])
+    print('printing checkedgroups')
     print(checkedgroups)
-        # if secgroup_name in checkedgroups:
-        #     # create_new_inst()
-        #     print('creating new instance')
-        #     break
-        # else:
-        #     print('lol')
-        #     sec_group = ec2.create_security_group(GroupName=secgroup_name, Description='slice_0 sec group')
-        #     sec_group.authorize_ingress(
-        #         CidrIp=myip,
-        #         IpProtocol='tcp',
-        #         FromPort=22,
-        #         ToPort=22
-        #     )   
-        #     sec_group.authorize_ingress(
-        #         CidrIp='0.0.0.0/0',
-        #         IpProtocol='tcp',
-        #         FromPort=80,
-        #         ToPort=80
-        #     )
-        #     print('group made, creating instance')
-        #     # create_new_inst()
-        #     break
+    if secgroup_name in checkedgroups:
+        # create_new_inst()
+        print('creating new instance')
+    else:
+        print('lol')
+        sec_group = ec2.create_security_group(GroupName=secgroup_name, Description='slice_0 sec group')
+        sec_group.authorize_ingress(
+            CidrIp=myip,
+            IpProtocol='tcp',
+            FromPort=22,
+            ToPort=22
+        )   
+        sec_group.authorize_ingress(
+            CidrIp='0.0.0.0/0',
+            IpProtocol='tcp',
+            FromPort=80,
+            ToPort=80
+        )
+        print('group made, creating instance')
+        # create_new_inst()
+        print(checkedgroups)
+        return sec_group
 
 get_instance_profile()
-handle_sec_groups()
+sec_group = handle_sec_groups(secgroup_name)
+# instance = create_new_inst(sec_group)
+# json.dumps(instance)
